c06d89c5d7e6463dad5e690e8b7112824fea01ea
Lucas281433 <lucas2814dev@gmail.com>
2024-12-11 11:03:41 -0300

    sixth commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@
  * @description Initializes the Express application and sets up middleware and routes.
  */
 const express = require("express");
-const path = require("path");
 const phonebookRouter = require("./controllers/phonebook");
 const middleware = require("./utils/middleware");
 const cors = require("cors");
@@ -13,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "/phonebook-client/dist")));
+app.use(express.static('dist'));
 app.use(middleware.requestLogger);
 app.use("/api/persons", phonebookRouter);
 app.use(middleware.unknownEndpoint);
